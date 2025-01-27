ee9463f7252c2797114cc86fe5cbec23cd3b366e
jaderAdriel <jaderadriel7@gmail.com>
2025-01-26 21:25:45 -0300

    Add configuration for H2 database, JWT keys, and initial role data

---
+++ backend/gym-system/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_roles(id, name) VALUES (1, 'ADMIN');
+INSERT INTO tb_roles(id, name) VALUES (2, 'RECEPTIONIST');
+INSERT INTO tb_roles(id, name) VALUES (3, 'PERSONAL_TRAINER');
+INSERT INTO tb_roles(id, name) VALUES (4, 'STUDENT');
